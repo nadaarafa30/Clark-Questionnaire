aa70e15ad9db25c55c8ba5e4accfea269e4ae693
nada Magdy <nada_magdy303@yahoo.com>
2019-06-19 00:14:37 +0200

    Main Structure Different Questionnait Types

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppComponent } from './app.component';
 import { RoutingModule } from 'src/app/routing/routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { QuestionnairComponent } from './components/questionnair/questionnair.component';
+import { FreeTextComponent } from './components/questionnair/free-text/free-text.component';
+import { MultiChoiceComponent } from './components/questionnair/multi-choice/multi-choice.component';
+import { SingleChoiceComponent } from './components/questionnair/single-choice/single-choice.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    QuestionnairComponent
+    QuestionnairComponent,
+    FreeTextComponent,
+    MultiChoiceComponent,
+    SingleChoiceComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/questionnair/free-text/free-text.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-free-text',
+  templateUrl: './free-text.component.html',
+  styleUrls: ['./free-text.component.scss']
+})
+export class FreeTextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
